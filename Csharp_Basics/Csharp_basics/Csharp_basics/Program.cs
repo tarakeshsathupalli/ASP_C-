3efcacb1a0aa39c077ead82685c6fe3cb93351e8
Subham <Subham@DESKTOP-EK3TDJ4>
2020-04-23 17:27:17 +0530

    do while and while loop

--- Csharp_Basics/Csharp_basics/Csharp_basics/Program.cs
+++ Csharp_Basics/Csharp_basics/Csharp_basics/Program.cs
@@ -7,35 +7,35 @@ namespace Csharp_basics
 
         static void Main(string[] args)
         {
-            /*  int? a = 10;
-              int x = a??0 ; //NUllCoalescen
-              bool b = (a == 10 ? true : false);
-              Console.WriteLine(b);
-              int? i = null; //coverting non nullable to nullable 
-              float d = 100.25f;
-              int r = (int)d;
-              int r1 = Convert.ToInt32(d);
-              Console.WriteLine(r1);
-              string sno = "1234";
-              int ino = int.Parse(sno); //covertion using parse method 
-              Console.WriteLine(ino);
-              string sno1 = "1234$";
-              int ino1 = 0;
-              if(int.TryParse(sno1, out ino1))
-                              ino1 = int.Parse(sno1);
-               else
-                  Console.WriteLine("conversion not possible");
-                  //covertion using parse method 
-              Console.WriteLine(ino1);*/
-            /*  arryas in C# *
-            int[] num = new int[3];
-                for (int j = 0; j < num.Length; j++)
-                    num[j] = j;
-                for (int j = 0; j < num.Length; j++)
-                    Console.WriteLine(num[j]);*/
-            /*if else statement */
-            labl:
-            Console.WriteLine("1. small  2.Medium  3.Large");
+        /*  int? a = 10;
+          int x = a??0 ; //NUllCoalescen
+          bool b = (a == 10 ? true : false);
+          Console.WriteLine(b);
+          int? i = null; //coverting non nullable to nullable 
+          float d = 100.25f;
+          int r = (int)d;
+          int r1 = Convert.ToInt32(d);
+          Console.WriteLine(r1);
+          string sno = "1234";
+          int ino = int.Parse(sno); //covertion using parse method 
+          Console.WriteLine(ino);
+          string sno1 = "1234$";
+          int ino1 = 0;
+          if(int.TryParse(sno1, out ino1))
+                          ino1 = int.Parse(sno1);
+           else
+              Console.WriteLine("conversion not possible");
+              //covertion using parse method 
+          Console.WriteLine(ino1);*/
+        /*  arryas in C# *
+        int[] num = new int[3];
+            for (int j = 0; j < num.Length; j++)
+                num[j] = j;
+            for (int j = 0; j < num.Length; j++)
+                Console.WriteLine(num[j]);*/
+        /*if else statement */
+        labl:
+            /*Console.WriteLine("1. small  2.Medium  3.Large");
             int a = int.Parse(Console.ReadLine());
             int totalBill = 0;
             /*if (a == 1)
@@ -44,7 +44,7 @@ namespace Csharp_basics
                 Console.WriteLine("user entered {0}", a);*/
             /*switch statement*/
 
-            switch (a)
+            /*switch (a)
             {
                 case 1:
                     totalBill += 1;
@@ -64,8 +64,26 @@ namespace Csharp_basics
              if (userchoice == "Y")
                  goto labl;
              else
-                Console.WriteLine("bill is {0}", totalBill);
-              
+                Console.WriteLine("bill is {0}", totalBill);*/
+
+            /* loops*/
+            /* while*/
+            Console.WriteLine("enter a number to print even numbers upto");
+            int no = int.Parse(Console.ReadLine());
+            int itr = 0;
+            /*while (itr<=no)
+            {
+                if (itr % 2 == 0)
+                    Console.WriteLine(itr);
+                itr++;
+            }*/
+
+            /* do while*/
+            do
+            {
+
+            }
+            while (itr <= no);
 
              
 
